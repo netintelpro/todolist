c129cc95ccaa68d0942c2cd6bc2fc1d8553963a6
netintelpro <quantumcas@gmail.com>
2016-04-24 23:03:14 -0500

    Update

---
+++ init.php
@@ -0,0 +1,9 @@
+<?php
+	session_start();
+	$hostname = "localhost";
+	$password = "pepper2016"
+	$username = "cp22771_todo";
+	$dbname = "cp22771_todolist";
+	$con = mysqli_connect($hostname,$username,$password,$dbname);
+	
+?>
